1ddb3e59c11bb3a8ce9a8338cf7f0abf6aa43a4a
chriswilliams1977 <chriswilliams1977@gmail.com>
2019-06-18 07:49:44 +0200

    changing node count

--- gkecluster.tf
+++ gkecluster.tf
@@ -2,5 +2,5 @@ resource "google_container_cluster" "gke-cluster" {
   name               = "terraform-gke-cluster"
   network            = "default"
   location           = "europe-west4-a"
-  initial_node_count = 4
+  initial_node_count = 3
 }
